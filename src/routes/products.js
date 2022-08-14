fa63a1b025a6d7631711bc0eaf61dee927c197ca
Zaki Maliki <malikizakitop@gmail.com>
2022-08-15 00:35:52 +0700

    update

--- src/routes/products.js
+++ src/routes/products.js
@@ -1,22 +1,9 @@
 
 const express = require('express')
 const router = express.Router()
-const multer = require('multer')
+const upload = require('../middlewares/upload')
 const {getAllProduct,getProduct,insertProduct,updateProduct,deleteProduct} = require('../controller/products')
 const {protect} = require('../middlewares/auth')
-
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, './upload')
-  },
-  filename: function (req, file, cb) {
-    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
-    cb(null, file.fieldname + '-' + uniqueSuffix + '-' + file.originalname)
-  }
-})
-
-const upload = multer({ storage: storage })
-
 
 router
   .get('/',protect, getAllProduct)
