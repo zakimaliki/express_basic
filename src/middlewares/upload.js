a5bbffbb7a342204095694d0dcd25e90b21e09f0
Zaki Maliki <malikizakitop@gmail.com>
2022-08-16 14:35:45 +0700

    update

--- src/middlewares/upload.js
+++ src/middlewares/upload.js
@@ -6,10 +6,11 @@ const storage = multer.diskStorage({
     },
     filename: function (req, file, cb) {
       const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
-      cb(null, file.fieldname + '-' + uniqueSuffix + '-' + file.originalname.replace(/\s/g, '-'))
+      cb(null, file.fieldname + '-' + uniqueSuffix+'.png')
     }
   })
   
   const upload = multer({ storage: storage })
+
 
   module.exports = upload
